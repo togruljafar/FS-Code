84ee0447ef7008652c58fba22fdaa8215b33d4f9
Togrul Jafar <60971206+togrul492@users.noreply.github.com>
2021-02-14 00:05:09 +0400

    Update task1.php

--- task-day1/task1.php
+++ task-day1/task1.php
@@ -1,24 +1,25 @@
 <?php
-    $sentence = " Bu cumlede tam-tamina alti  soz var. ";
+    $sentence = " Bu cumlede tam-tamina alti  soz var.. ";
     $count = 0;
-    //olasi artiq bosluqlari silmek ucun
-    $sentence = trim($sentence);
-    // cumlemi sozlere parcalayiram ve words arrayine elave edirem
+    $sentence = trim($sentence); // olasi artiq bosluqlari sifirlamaq ucun
+    // cumlemi sozlere parcalayriram ve words arrayine elave edirem
     $word = '';
     $words = [];
 
     for($i = 0; isset($sentence[$i]) ; $i++) {
         if ($sentence[$i] == " " || $sentence[$i] == ".") {
             $word = trim($word);
-            if(!empty($word)) {
+            if(!empty($word) && $word != ".") {
                 $words[] = $word;
-                $word = '';
             }
+            $word = '';
         }
         $word .= $sentence[$i];
     }
     
     foreach($words as $value) {
+        echo $value."<br>";
+        
         $count++;
     }
 
